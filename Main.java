ca3b4c1131d49ab24d57b7146b43e3f44cafce50
George Salib <salibgeorge91372@gmail.com>
2020-11-19 00:12:18 +0100

    created list of all fibonacci numbers under 4M

--- Main.java
+++ Main.java
@@ -7,7 +7,11 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	// Project euler (Problem 1)
+    // Project euler (Problem 1)
+        /* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+        Find the sum of all the multiples of 3 or 5 below 1000. */
+
 
         List<Integer> multiplesOf3 = new ArrayList<Integer>();
         List<Integer> multiplesOf5 = new ArrayList<Integer>();
