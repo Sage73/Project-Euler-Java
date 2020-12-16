896fafb26f16408cf342027ec6ea5f54ce0925f5
George Salib <salibgeorge91372@gmail.com>
2020-12-16 19:32:30 +0100

    completed problem 6

--- Problem6.java
+++ Problem6.java
@@ -1,14 +1,28 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
 
 public class Problem6 {
     public static void main(String[] args) {
         /* Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 -385 = 2640
         Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum. */
 
-        ArrayList <Integer> sumOfSquareIntegers = new ArrayList<>();
-        ArrayList <Integer> squareOfSum = new ArrayList<>();
-        
+        ArrayList <Integer> sumOfSquares = new ArrayList<>();
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sumOfSquares.add((int) Math.pow(i, 2));
+        }
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+
+        int sumOfSquare = sumOfSquares.stream().mapToInt(Integer::intValue).sum();
+        int squareOfSum = (int) Math.pow(sum, 2);
+
+        int result = squareOfSum - sumOfSquare;
+        System.out.println(result);
     }
 }
